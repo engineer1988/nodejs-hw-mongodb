78d87e9a10650378c53a35bc050e5451024367fa
Dmytro Maniuta <ip722428@gmail.com>
2024-06-02 14:47:05 +0300

    finished step 4

---
+++ src/db/student.js
@@ -0,0 +1,34 @@
+import { model, Schema } from 'mongoose';
+
+const studentsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: false,
+    },
+    isFavourite: {
+      type: String,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      required: true,
+      enum: ['work', 'home', 'personal'],
+      default: 'personal',
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const Contact = model('students', studentsSchema);
