94fb9c0f7c559de279dfe25fd78d8cd12fbb8f6d
Dmytro Maniuta <ip722428@gmail.com>
2024-06-04 15:04:54 +0300

    hw-2

--- src/server.js
+++ src/server.js
@@ -51,13 +51,13 @@ export const setupServer = () => {
     }),
   );
 
-  app.use('*', (req, res, next) => {
+  app.use('*', (req, res) => {
     res.status(404).json({
       message: 'Not found',
     });
   });
 
-  app.use((err, req, res, next) => {
+  app.use((err, req, res) => {
     res.status(500).json({
       message: 'Something went wrong',
       error: err.message,
