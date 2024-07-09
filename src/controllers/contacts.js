cab1ddf48711cb7c27c9182e8512432c90289ff5
Dmytro Maniuta <ip722428@gmail.com>
2024-07-09 21:56:37 +0300

    finished hw-7

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -47,16 +47,6 @@ export const getContactByIdController = async (req, res, next) => {
   }
 };
 
-// export const createContactController = async (req, res) => {
-//   const contact = await createContact({ ...req.body, userId: req.user._id });
-
-//   res.status(201).json({
-//     status: 201,
-//     message: `Successfully created a contact!`,
-//     data: contact,
-//   });
-// };
-
 export const createContactController = async (req, res) => {
   const photo = req.file;
   let photoUrl;
